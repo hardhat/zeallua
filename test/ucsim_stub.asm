daa4a523c0511f1469eceeeeb021ab0db267dfb6
Dale Wick <dmwick@yahoo.com>
2026-03-17 22:54:14 -0400

    First test assignment

--- test/ucsim_stub.asm
+++ test/ucsim_stub.asm
@@ -1,36 +1,67 @@
 ; ucsim_stub.asm
-; A basic test stub to simulate the Zeal-8-bit-OS kernel for ucsim_z80
-    .area _BOOT (ABS)
+; Minimal RST 08h trap for compiler regression tests under ucsim.
 
-    .org 0x0000
-    jp 0x4000       ; Jump to user application loaded by SDCC at 0x4000
+    org 0x0000
+    jp _boot_up
 
-    .org 0x0008
-    ; Syscall entry point (RST 08h)
-    ; L contains the syscall number.
-    ; For automated testing, we might trap here or simulate limited I/O.
-    ld a, l
-    cp #0           ; sys_read
-    jr z, _sys_read
-    cp #1           ; sys_write
-    jr z, _sys_write
-    cp #15          ; sys_exit
+    defs 0x0008 - $, 0
+_syscall:
+    cp 0            ; exit
     jr z, _sys_exit
-    ; Unknown syscall or unhandled
-    halt
-
-_sys_read:
-    ; Not yet implemented in stub
+    cp 2            ; write(dev=B, buf=DE, size_ptr=HL)
+    jr z, _sys_write
+    xor a
     ret
 
 _sys_write:
-    ; A simple mock to "write" to the simulator.
-    ; Often simulators capture writes to specific I/O ports for console out.
-    ; For ucsim, we can just return success for now.
-    xor a           ; return ERR_SUCCESS
+    ld a, b         ; Only capture stdout
+    cp 0
+    jr nz, _sys_write_done
+
+    ld c, (hl)
+    inc hl
+    ld b, (hl)
+
+_sys_write_loop:
+    ld a, c
+    or b
+    jr z, _sys_write_done
+    ld a, (de)
+    call _append_output
+    inc de
+    dec bc
+    jr _sys_write_loop
+
+_sys_write_done:
+    xor a
+    ret
+
+_append_output:
+    push hl
+    ld hl, (_output_ptr)
+    ld (hl), a
+    inc hl
+    ld (_output_ptr), hl
+    xor a
+    ld (hl), a
+    pop hl
     ret
 
 _sys_exit:
-    ; Exit the simulator
+    xor a
     halt
 
+_boot_up:
+    ld sp, 0xFFFE
+    ld hl, _OUTPUT_BUFFER
+    ld (_output_ptr), hl
+    xor a
+    ld (_OUTPUT_BUFFER), a
+    jp 0x4000
+
+_output_ptr:
+    dw _OUTPUT_BUFFER
+
+    defs 0x0200 - $, 0
+_OUTPUT_BUFFER:
+    defs 256, 0
